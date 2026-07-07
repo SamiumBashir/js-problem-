db79d97860989619f412aaab566f7ef36be8ff94
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-07-07 09:38:04 +0600

    solving problem with function

---
+++ self_practice/practice_2.js
@@ -0,0 +1,16 @@
+let sentence = 'i am loving javascript';
+
+function changeUpCase (text){
+    let words = sentence.split(' ');
+    let modifiedText = [];
+
+    for(const word of words){
+        let capitalizedWord = word[0].toUpperCase() + word.slice(1).toLowerCase();
+        modifiedText.push(capitalizedWord);
+    }
+    let finalSentence = modifiedText.join(' ');
+    return finalSentence;
+}
+
+const finalText = changeUpCase(sentence);
+console.log(finalText);
